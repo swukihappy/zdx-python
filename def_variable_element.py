2d0abe0a14c3828242c68ba2424a7b31963f355a
root <root@DESKTOP-MEF8TFF.localdomain>
2018-04-25 22:24:54 +0800

    可变参数练习加调用

--- def_variable_element.py
+++ def_variable_element.py
@@ -6,3 +6,22 @@ def product(x, *kw):
     for n in kw:
         sum=sum*n
     return sum
+# 测试
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
